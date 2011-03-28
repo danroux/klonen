fc742fb4ed033df53435ef31a245c977a6044da2
Daniel Roux <daniel.roux@gmail.com>
2011-03-28 17:50:20 -0600

    * Added a function as second parameter that yields the newElement for customization.
    * New structure of the code. Work in progress.
    
    * There's a bug that generates an error when the function runs for the first time. Working on it.
    
    * Now the plugin works on the element you want to clone, no need for the base_element option anymore.
    
    TODO: cleane the newElement fields.

--- jquery.klonen.js
+++ jquery.klonen.js
@@ -3,35 +3,42 @@
  * http://github.com/danroux/klonen
  *
  * Copyright (c) 2011 Daniel Roux
-*
+ *
  * Licensed under the MIT:
  *   http://www.opensource.org/licenses/mit-license.php
  */
 
 (function($){
-    $.fn.klonen = function(options){
+    $.fn.klonen = function(options, callback){
         var settings = {
             add_link: "Add more",
             remove_link: "Remove",
-            base_element: "",
             insert_after: "",
             get_length:    0
         }
-        var that = this;
+
         if ( options ) { 
             $.extend( settings, options );
         }
 
-        var addLink = setAddLink(), 
-            removeLink = setRemoveLink();
+        var baseElement = this;
+
+        return function(){
+            var  addLink = setAddLink(), 
+                 removeLink = setRemoveLink(),
+                 newElement;            
+            var linksContainer = $('<div>').append(addLink).append(' ').append(removeLink);
+            linksContainer.appendTo($(baseElement).parent());
+            return baseElement;
+        }();
 
         function setAddLink(){
             addLink = $('<a href="#" >' + settings.add_link + '</a>');
             addLink.click(function(){
-                var new_element = $(settings.base_element).clone();
-
-                plusLength(new_element);
-                new_element.insertAfter(settings.insert_after);
+                newElement = $(baseElement).clone();
+                plusLength(newElement);
+                bindCallbacks(newElement);
+                newElement.insertAfter(settings.insert_after);
                 return false;
             });
             return addLink;
@@ -40,15 +47,15 @@
         function setRemoveLink(){
             var link = $('<a href="">' + settings.remove_link + '</a>');
             link.click(function(){
-                that.children(settings.get_length + ':not(:first):last').remove();
+                baseElement.children(settings.get_length + ':not(:first):last').remove();
                 return false;
             });
             return link;
         }
         
-        function plusLength(new_element){
+        function plusLength(newElement){
             var length = $(settings.get_length).length;
-            $(new_element).find('li, label, input', 'select').each(function(){
+            $(newElement).find('li, label, input, select').each(function(){
                 var attributes = ["id"];
                 var that = $(this);
                 var tagName = this.tagName.toLowerCase();
@@ -60,17 +67,19 @@
                 case "input":
                     attributes.push("name");
                     break;
+                case "select":
+                    attributes.push("name");
+                    break;
                 }
-
                 $.each(attributes, function(index, value){
                     var attr = $(that).attr(value);
                     $(that).attr(value, attr.replace(/\d/, length));
                 });
             });
         }
-        
-        var links_container = $('<div>').append(addLink).append(' ').append(removeLink);
-        links_container.appendTo(this);
-        return links_container;
-    }
-})(jQuery)
+
+        function bindCallbacks(newElement){
+            if(typeof(callback) == 'function') callback(newElement);
+        }
+    };
+})(jQuery);
